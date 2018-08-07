d05caa73c5ef2f1862e4d08ea41594021f87a368
Hannah Spencer <hannahlspencer@gmail.com>
2018-08-07 14:30:05 +0100

    remove playsoundstation

--- playsoundstation/src/Genre/Genre.java
+++
@@ -1,9 +0,0 @@
-package Genre;
-
-public interface Genre {
-    String[] bassNoteLengths = new String[0];
-    String[] instruments = new String[0];
-
-    String[] getBassNoteLengths();
-    String[] getInstruments();
-}
