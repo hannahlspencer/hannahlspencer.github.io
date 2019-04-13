f1fb6bd662036e2445c3db871ec5ccfc0d6feae7
Hannah Spencer <hannah.spencer@bbc.co.uk>
2019-04-13 13:59:29 +0100

    full redesign

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "95%";
+  document.getElementById("myNav").style.border = "1px solid black";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("myNav").style.border = "";
+}
 
 $(document).ready(() => {
   
   //makes hero content fade in
-  $('#intro-name').hide().fadeIn(2000);
-
-  $('#front').hide().fadeIn(4000);
-  $('#mobile').hide().fadeIn(5000);
-  $('#ai').hide().fadeIn(6000);
+  $('#intro-name').hide().fadeIn(4000);
   
   //$('#panel')
   $('.accordion').on('click', event => {
